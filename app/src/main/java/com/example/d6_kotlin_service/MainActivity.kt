93d0d67ec0514b4b42443340573eceddf23db1c1
jv1111 <webd0829@gmail.com>
2023-07-04 10:16:11 +0800

    binding

--- app/src/main/java/com/example/d6_kotlin_service/MainActivity.kt
+++ app/src/main/java/com/example/d6_kotlin_service/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.d6_kotlin_service
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.d6_kotlin_service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener{
+
+        }
+        binding.btnStop.setOnClickListener{
+
+        }
+
     }
 }
